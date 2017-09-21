b7dbf82dda59fc7cd112c023838addd259cbce2d
jackdoresky <jdoresky@andrew.cmu.edu>
2017-09-21 08:02:04 -0400

    commit to master

--- app.rb
+++ app.rb
@@ -31,8 +31,22 @@ get "/sms/incoming" do
   if session["counter"] == 1
     message = "Thanks for your first message. From #{sender} saying #{body}"
     media = "https://media.giphy.com/media/13ZHjidRzoi7n2/giphy.gif"
+  end
+  if body == "What are you doing" || body == "wryd"
+    message = "I'm at the gym lol. Why?"
+    sleep (2)
+    message = "Actually nvm. Idc lol"
+  end
+  if body == "Have you done the hw yet?"
+    message = "Yeah a while ago."
+    if == "Can you help me"
+      message = "No"
+    else == "We're done here."
+    end
+  end
+
   else
-    message = "Thanks for message number #{ count }. From #{sender} saying #{body}"
+    message = "I am sleeping. Don't bother me rn."
     media = nil
   end
 
